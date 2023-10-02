b8a52a7d10bf54a783728f13bc8aa77644a0d100
Minkyu Lee <whizkyu@snu.ac.kr>
2023-10-02 19:18:59 +0900

    CD 움직임/회전에 spring 효과 적용

--- cycle1/disk-on/components/disk/Disk.tsx
+++ cycle1/disk-on/components/disk/Disk.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import styles from "./Disk.module.css";
-import { useState } from "react";
+import { useSpring, animated } from "@react-spring/web";
 
 type DiskProps = {
   size?: number;
@@ -11,41 +11,46 @@ function Disk({ size = 220 }: DiskProps) {
   const cdSize220 = size;
   const cdSize216 = (size * 216) / 220;
 
-  const [translateX, setTranslateX] = useState(0);
-  const [translateY, setTranslateY] = useState(0);
-  const [translateZ, setTranslateZ] = useState(0);
-  const [rotateX, setRotateX] = useState(0);
-  const [rotateY, setRotateY] = useState(0);
-  const [rotateZ, setRotateZ] = useState(0);
+  const [translate, translateApi] = useSpring(() => ({
+    from: { "--translateX": "0", "--translateY": "0", "--translateZ": "0" },
+  }));
+
+  const [rotate, rotateApi] = useSpring(() => ({
+    from: { "--rotateX": "0", "--rotateY": "0", "--rotateZ": "0" },
+  }));
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const rect = e.currentTarget.getBoundingClientRect();
     const relativeX = ((e.clientX - rect.left - size / 2) / size) * 2;
     const relativeY = ((e.clientY - rect.top - size / 2) / size) * -2;
-    console.log(relativeX, relativeY);
-    setRotateX(-relativeY * 30);
-    setRotateY(-relativeX * 30);
+    rotateApi.start({
+      to: {
+        "--rotateX": `${-relativeY * 30}deg`,
+        "--rotateY": `${-relativeX * 30}deg`,
+        "--rotateZ": "0",
+      },
+    });
   };
 
   const handleMouseLeave = () => {
-    setRotateX(0);
-    setRotateY(0);
+    rotateApi.start({
+      to: {
+        "--rotateX": "0",
+        "--rotateY": "0",
+        "--rotateZ": "0",
+      },
+    });
   };
 
   return (
-    <div
+    <animated.div
       className={styles.scene}
       style={{
         width: cdSize220,
         height: cdSize220,
         perspective: "1000px",
-        // @ts-ignore
-        "--translateX": `${translateX}px`,
-        "--translateY": `${translateY}px`,
-        "--translateZ": `${translateZ}px`,
-        "--rotateX": `${rotateX}deg`,
-        "--rotateY": `${rotateY}deg`,
-        "--rotateZ": `${rotateZ}deg`,
+        ...translate,
+        ...rotate,
       }}
       onMouseMove={handleMouseMove}
       onMouseLeave={handleMouseLeave}
@@ -83,7 +88,7 @@ function Disk({ size = 220 }: DiskProps) {
           </div>
         </div>
       </div>
-    </div>
+    </animated.div>
   );
 }
 
