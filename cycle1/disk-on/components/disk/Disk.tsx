20fa6a9b99005e602f97e81e1f8058579f2b0df2
Minkyu Lee <whizkyu@snu.ac.kr>
2023-10-02 18:57:15 +0900

    마우스 위치에 따라 회전하는 CD 구현

--- cycle1/disk-on/components/disk/Disk.tsx
+++ cycle1/disk-on/components/disk/Disk.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import styles from "./Disk.module.css";
+import { useState } from "react";
 
 type DiskProps = {
   size?: number;
@@ -8,6 +11,27 @@ function Disk({ size = 220 }: DiskProps) {
   const cdSize220 = size;
   const cdSize216 = (size * 216) / 220;
 
+  const [translateX, setTranslateX] = useState(0);
+  const [translateY, setTranslateY] = useState(0);
+  const [translateZ, setTranslateZ] = useState(0);
+  const [rotateX, setRotateX] = useState(0);
+  const [rotateY, setRotateY] = useState(0);
+  const [rotateZ, setRotateZ] = useState(0);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const relativeX = ((e.clientX - rect.left - size / 2) / size) * 2;
+    const relativeY = ((e.clientY - rect.top - size / 2) / size) * -2;
+    console.log(relativeX, relativeY);
+    setRotateX(-relativeY * 30);
+    setRotateY(-relativeX * 30);
+  };
+
+  const handleMouseLeave = () => {
+    setRotateX(0);
+    setRotateY(0);
+  };
+
   return (
     <div
       className={styles.scene}
@@ -16,16 +40,15 @@ function Disk({ size = 220 }: DiskProps) {
         height: cdSize220,
         perspective: "1000px",
         // @ts-ignore
-        "--translateX": "0px",
-        "--translateY": "0px",
-        "--translateZ": "0px",
-        "--rotateX": "30deg",
-        "--rotateY": "45deg",
-        "--rotateZ": "0deg",
-        "--scaleX": "1",
-        "--scaleY": "1",
-        "--scaleZ": "1",
+        "--translateX": `${translateX}px`,
+        "--translateY": `${translateY}px`,
+        "--translateZ": `${translateZ}px`,
+        "--rotateX": `${rotateX}deg`,
+        "--rotateY": `${rotateY}deg`,
+        "--rotateZ": `${rotateZ}deg`,
       }}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
     >
       <svg>
         <defs>
