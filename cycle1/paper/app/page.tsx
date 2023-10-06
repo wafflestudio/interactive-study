41bbebcc252bc0adf36d20335dde516d4887746d
lerrybe <lerry597@gmail.com>
2023-10-07 00:56:43 +0900

    feat: implement intro page

--- cycle1/paper/app/page.tsx
+++ cycle1/paper/app/page.tsx
@@ -1,7 +1,34 @@
+'use client';
+
+import { useCallback, useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Wallet from '@/components/wallet';
+import StageAnnounce from '@/components/stage-announce';
+
 export default function Home() {
+  const start = 500;
+  const end = 6000;
+  const router = useRouter();
+  const [step, setStep] = useState(0);
+
+  const onNext = useCallback(() => {
+    router.push('/game');
+  }, [router]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setStep(1);
+    }, end);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <main>
-      <h1>Page</h1>
-    </main>
-  )
+    <>
+      {step === 0 && <Wallet start={start} end={end} />}
+      {step === 1 && <StageAnnounce onNext={onNext} />}
+    </>
+  );
 }
