5aec87886347a7ceefced4256837efaca1b051bd
lerrybe <yeji.kim@dalpha.so>
2024-01-08 22:51:24 +0900

    feat: 기본 세팅

--- projects/waffle-sans/src/main.tsx
+++ projects/waffle-sans/src/main.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Reset } from 'styled-reset';
 
 import App from './App.tsx';
-import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Reset />
     <App />
   </React.StrictMode>,
 );
---
+++ projects/waffle-sans/src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from '../pages/Home';
+
+export default function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
