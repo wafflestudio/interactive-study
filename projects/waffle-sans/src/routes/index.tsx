c4857ad42ce72677baab4fd9f989ffb4ddde87a0
lerrybe <yeji.kim@dalpha.so>
2024-01-09 01:35:38 +0900

    feat: implements sans writer page

--- projects/waffle-sans/src/routes/index.tsx
+++ projects/waffle-sans/src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home';
+import Sans from '../pages/Sans';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/sans" element={<Sans />} />
       </Routes>
     </BrowserRouter>
   );
