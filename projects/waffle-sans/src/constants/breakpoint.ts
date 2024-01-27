3d8354363aae230d956a584c3455c98011956b9f
lerrybe <yeji.kim@dalpha.so>
2024-01-27 18:53:31 +0900

    feat: home

--- projects/waffle-sans/src/constants/breakpoint.ts
+++ projects/waffle-sans/src/constants/breakpoint.ts
@@ -1,7 +1,5 @@
 export const GRID = {
-  XL_DESKTOP: 'only screen and (min-width: 1921px)',
-  L_DESKTOP: 'only screen and (min-width: 1441px)', // 1440 ~ 1920 (XL 추가시 max-width: 1920px 추가)
-  DESKTOP: 'only screen and (min-width: 1025px) and (max-width: 1440px)',
-  TABLET: 'only screen and (min-width: 769px) and (max-width: 1024px)',
-  MOBILE: 'only screen and (max-width: 768px)',
+  DESKTOP: 'only screen and (min-width: 1025px)',
+  TABLET: 'only screen and (min-width: 712px) and (max-width: 1024px)',
+  MOBILE: 'only screen and (max-width: 711px)',
 };
