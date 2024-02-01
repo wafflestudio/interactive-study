d937a9e3deb3a9b5f4d5db48838abaf2d98baf00
Minkyu Lee <whizkyu@snu.ac.kr>
2024-02-01 23:29:25 +0900

    fix: 배포 시 이미지 prefix 누락 이슈 수정

--- projects/paper/constants/imgUrls.ts
+++ projects/paper/constants/imgUrls.ts
@@ -1,4 +1,6 @@
-export const rightHolo = '/images/masks/right_holo_mask.svg';
-export const centerHolo = '/images/masks/center_holo_mask.svg';
-export const leftHolo = '/images/masks/genuine_left_holo_mask.svg';
-export const fakeLeftHolo = '/images/masks/fake3_left_holo_mask.svg';
+import { basePath } from '../next.config';
+
+export const rightHolo = `${basePath}/images/masks/right_holo_mask.svg`;
+export const centerHolo = `${basePath}/images/masks/center_holo_mask.svg`;
+export const leftHolo = `${basePath}/images/masks/genuine_left_holo_mask.svg`;
+export const fakeLeftHolo = `${basePath}/images/masks/fake3_left_holo_mask.svg`;
