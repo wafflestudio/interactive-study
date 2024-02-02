16ef193e470448b58c524dd472cb1e4c3098efea
designDefined <designdefined.by@gmail.com>
2024-02-02 22:15:12 +0900

    feat: hide background when letter is out

--- projects/waffle-sans/src/components/Letter.tsx
+++ projects/waffle-sans/src/components/Letter.tsx
@@ -15,33 +15,43 @@ export default function Letter() {
   }, [stage]);
 
   return (
-    <Container onClick={onClickLetter} $stage={stage}>
-      <LetterBack $isOut={stage === 'out'} />
-      <PaperWrapper $isOut={stage === 'out'}>
-        <ReceivedContent
-          sansContent="interactive study"
-          from="Interactive Study"
-        />
-      </PaperWrapper>
-      <LetterFront src="letter_front_main_blue.png" $isOut={stage === 'out'} />
-      <LetterFront
-        src="letter_front_bottom_blue.png"
-        $isOut={stage === 'out'}
-      />
-      <LetterCoverWrapper $isOpen={stage === 'open'} $isOut={stage === 'out'}>
-        <LetterCoverInside
-          src="letter_cover_inside_blue.png"
-          $isOpen={stage === 'open' || stage === 'out'}
+    <>
+      <BackgroundHider $isOut={stage === 'out'} />
+      <Container onClick={onClickLetter} $stage={stage}>
+        <LetterBack $isOut={stage === 'out'} />
+        <PaperWrapper $isOut={stage === 'out'}>
+          <ReceivedContent
+            sansContent="interactive study"
+            from="Interactive Study"
+          />
+        </PaperWrapper>
+        <LetterFront
+          src="letter_front_main_blue.png"
           $isOut={stage === 'out'}
         />
-        <LetterCoverOutside
-          src="letter_cover_outside_blue.png"
-          $isOpen={stage === 'open' || stage === 'out'}
+        <LetterFront
+          src="letter_front_bottom_blue.png"
           $isOut={stage === 'out'}
         />
-        <Ribbon $isGone={stage !== 'shake'} src="letter_ribbon.png" />
-      </LetterCoverWrapper>
-    </Container>
+        <LetterCoverWrapper $isOpen={stage === 'open'} $isOut={stage === 'out'}>
+          <LetterCoverInside
+            src="letter_cover_inside_blue.png"
+            $isOpen={stage === 'open' || stage === 'out'}
+            $isOut={stage === 'out'}
+          />
+          <LetterCoverOutside
+            src="letter_cover_outside_blue.png"
+            $isOpen={stage === 'open' || stage === 'out'}
+            $isOut={stage === 'out'}
+          />
+          <Ribbon
+            $isGone={stage !== 'shake'}
+            $isOpen={stage === 'open' || stage === 'out'}
+            src="letter_ribbon.png"
+          />
+        </LetterCoverWrapper>
+      </Container>
+    </>
   );
 }
 
@@ -62,6 +72,19 @@ const Container = styled.div<{ $stage: 'shake' | 'close' | 'open' | 'out' }>`
     css`
       animation: ${shake} 0.2s infinite alternate linear;
     `}
+`;
+
+const BackgroundHider = styled.div<{ $isOut: boolean }>`
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  background-color: #475e65;
+  pointer-events: none;
+  transition: 1s ease;
+  opacity: ${({ $isOut }) => ($isOut ? 1 : 0)};
+  z-index: 1;
 `;
 
 const LetterBack = styled.div<{ $isOut: boolean }>`
@@ -85,6 +108,7 @@ const LetterFront = styled.img<{ $isOut: boolean }>`
   opacity: ${({ $isOut }) => ($isOut ? 0 : 1)};
 
   filter: drop-shadow(0px -6px 6px rgba(0, 0, 0, 0.05));
+  pointer-events: none;
 `;
 
 const LetterCoverWrapper = styled.div<{ $isOpen: boolean; $isOut: boolean }>`
@@ -107,6 +131,7 @@ const LetterCoverWrapper = styled.div<{ $isOpen: boolean; $isOut: boolean }>`
 
   opacity: ${({ $isOut }) => ($isOut ? 0 : 1)};
   z-index: ${({ $isOut }) => ($isOut ? -1 : 1)};
+  pointer-events: none;
 `;
 
 const LetterCoverInside = styled.img<{ $isOpen: boolean; $isOut: boolean }>`
@@ -125,13 +150,14 @@ const LetterCoverOutside = styled.img<{ $isOpen: boolean; $isOut: boolean }>`
   filter: drop-shadow(0px 6px 6px rgba(0, 0, 0, 0.1));
 `;
 
-const Ribbon = styled.img<{ $isGone: boolean }>`
+const Ribbon = styled.img<{ $isGone: boolean; $isOpen: boolean }>`
   position: relative;
   width: 55%;
   opacity: ${({ $isGone }) => ($isGone ? 0 : 1)};
   transform: translateY(24%);
   transition: 1s ease;
   z-index: 1;
+  ${({ $isOpen }) => $isOpen && `display: none;`}
 `;
 
 const PaperWrapper = styled.div<{ $isOut: boolean }>`
--- projects/waffle-sans/src/components/ReceivedContent.tsx
+++ projects/waffle-sans/src/components/ReceivedContent.tsx
@@ -1,6 +1,6 @@
-import createWreathSans from 'leon-sans-react/src/hooks/createWreathSans';
-import { useEffect } from 'react';
 import styled from 'styled-components';
+
+import useWreathSans from '../hooks/useWreathSans';
 
 type Props = {
   sansContent: string;
@@ -8,28 +8,13 @@ type Props = {
 };
 
 export default function ReceivedContent({ sansContent, from }: Props) {
-  const { WreathSansCanvas, resize } = createWreathSans({
-    initialText: sansContent,
-    width: window.innerWidth - 100,
-    height: 200,
-    size: 40,
-    color: '#704234',
-    background: 'transparent',
-  });
-
-  useEffect(() => {
-    function handleResize() {
-      resize(window.innerWidth, (window.innerHeight / 100) * 62);
-    }
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, [resize]);
+  const { ref, WreathSansCanvas } = useWreathSans({ initialText: sansContent });
 
   return (
     <Container>
-      <WreathSansCanvas />
+      <SansWrapper ref={ref}>
+        <WreathSansCanvas />
+      </SansWrapper>
       <MainText>
         메리 크리스마스 <br />
         즐거운 성탄절 보내세요 하하. 메리 크리스마스 즐거운 성탄절 보내세요
@@ -45,16 +30,15 @@ export default function ReceivedContent({ sansContent, from }: Props) {
 }
 
 const Container = styled.div`
-  position: absolute;
-  top: 10px;
-  left: 0;
+  position: relative;
   width: 100%;
   aspect-ratio: 0.55;
+  box-sizing: border-box;
 
+  padding: 45px 40px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 20px;
 
   background-size: 300%;
   background-position: right;
@@ -62,13 +46,19 @@ const Container = styled.div`
   box-shadow: 0 6px 6px 0 rgba(0, 0, 0, 0.15);
 `;
 
+const SansWrapper = styled.div`
+  width: 100%;
+`;
+
 const MainText = styled.div`
   color: #315c57;
 
-  width: 60%;
+  width: 100%;
   height: 208px;
   flex-shrink: 0;
   overflow-y: auto;
+
+  margin-top: 60px;
 
   text-align: justify;
   font-family: Inter;
