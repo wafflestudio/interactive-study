1f3b9f874dd932c4324ad8a0b4a69c4036b215ff
Minkyu Lee <whizkyu@snu.ac.kr>
2024-02-02 20:16:49 +0900

    refactor: base 경로 설정 로직 이동

--- projects/waffle-sans/vite.config.ts
+++ projects/waffle-sans/vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({mode}) => ({
   plugins: [react()],
-});
+  base: mode === "production" ? "/projects/waffle-sans/" : "/",
+}));
