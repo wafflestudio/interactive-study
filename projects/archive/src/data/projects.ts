ab080ab9271321672623a0f535bb08f4e3a3b154
designDefined <designdefined.by@gmail.com>
2024-02-03 16:54:24 +0900

    Fix: 링크오류

--- projects/archive/src/data/projects.ts
+++ projects/archive/src/data/projects.ts
@@ -15,7 +15,7 @@ const projectData: ProjectData[] = [
       '마우스를 이용해 화면 속의 CD 인터랙티브를 즐겨보세요!\n진짜 CD처럼 보이겠지만, 마음껏 다뤄도 절대 깨지지 않아요.',
     thumbnailSrc: `${import.meta.env.BASE_URL}disk-on_thumbnail.png`,
     videoSrc: `${import.meta.env.BASE_URL}disk-on_video.mov`,
-    url: 'https://interactive-study.wafflestudio.com/projects/paper/',
+    url: 'https://interactive-study.wafflestudio.com/projects/disk-on/',
   },
   {
     id: 1,
@@ -24,7 +24,7 @@ const projectData: ProjectData[] = [
       'SPOT FAKE는 지폐의 틀린 그림을 찾는 게임입니다. 세가지 조명모드를 활용하여\n 지폐를 샅샅이 살펴보고, 위조 지폐를 가려내세요! ',
     thumbnailSrc: `${import.meta.env.BASE_URL}spot-fake_thumbnail.png`,
     videoSrc: `${import.meta.env.BASE_URL}spot-fake_video.mp4`,
-    url: 'https://interactive-study.wafflestudio.com/projects/disk-on/',
+    url: 'https://interactive-study.wafflestudio.com/projects/paper/',
   },
   {
     id: 2,
