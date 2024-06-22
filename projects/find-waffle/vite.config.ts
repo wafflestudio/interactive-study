466f6e17b88aa7ee169f3e6bed18692e66e36444
Minkyu Lee <whizkyu@snu.ac.kr>
2024-06-22 13:41:55 +0900

    feat: find-waffle 프로젝트 초기 설정

---
+++ projects/find-waffle/vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({mode}) => ({
+  base: mode === "production" ? "/projects/find-waffle/" : "/",
+}));
