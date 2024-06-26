e4f1f4a0b58155433256273793cff46028d64726
nuagenic <nuagenic@gmail.com>
2024-06-26 18:48:38 +0900

    feat: Add Raycaster class and example

---
+++ projects/find-waffle/src/example/raycaster/main.ts
@@ -0,0 +1,88 @@
+import * as THREE from 'three';
+
+import { EventCallback, Raycaster } from '../../libs/raycaster/Raycaster.ts';
+
+/* Scene Settings */
+const canvas = document.createElement('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.position = 'absolute';
+canvas.style.top = '0';
+canvas.style.left = '0';
+canvas.style.margin = '0';
+canvas.style.padding = '0';
+document.body.appendChild(canvas);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const geometry = new THREE.BoxGeometry();
+const material1 = new THREE.MeshBasicMaterial({ color: '#00ff00' });
+const material2 = new THREE.MeshBasicMaterial({ color: '#00ff00' });
+const material3 = new THREE.MeshBasicMaterial({ color: '#00ff00' });
+const cube1 = new THREE.Mesh(geometry, material1);
+const cube2 = new THREE.Mesh(geometry, material2);
+const cube3 = new THREE.Mesh(geometry, material3);
+cube1.position.x = -3;
+cube2.position.x = 0;
+cube3.position.x = 3;
+scene.add(cube1);
+scene.add(cube2);
+scene.add(cube3);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube1.rotation.x += 0.01;
+  cube1.rotation.y += 0.01;
+
+  cube2.rotation.x += 0.01;
+  cube2.rotation.y += 0.01;
+
+  cube3.rotation.x += 0.01;
+  cube3.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+/* Raycaster Settings */
+const raycaster = new Raycaster(camera, scene);
+
+const mouseMoveCallback: EventCallback = (
+  event: MouseEvent,
+  intersects: THREE.Intersection[],
+) => {
+  if (intersects.length > 0) {
+    intersects[0].object.material.color.set('#ff0000');
+  } else {
+    cube1.material.color.set(0x00ff00);
+    cube2.material.color.set(0x00ff00);
+    cube3.material.color.set(0x00ff00);
+  }
+};
+
+const clickCallback: EventCallback = (
+  event: MouseEvent,
+  intersects: THREE.Intersection[],
+) => {
+  if (intersects.length > 0) {
+    console.log('Clicked', intersects[0].object);
+  }
+};
+
+raycaster.setMouseMoveHandler(mouseMoveCallback);
+raycaster.setClickHandler(clickCallback);
+
+window.addEventListener('mousemove', (event) => raycaster.onMouseMove(event));
+window.addEventListener('click', (event) => raycaster.onClick(event));
---
+++ projects/find-waffle/src/libs/raycaster/Raycaster.ts
@@ -0,0 +1,63 @@
+import * as THREE from 'three';
+
+type MouseCoords = THREE.Vector2;
+export type EventCallback = (
+  event: MouseEvent,
+  intersects: THREE.Intersection[],
+) => void;
+
+export class Raycaster extends THREE.Raycaster {
+  camera: THREE.Camera;
+  scene: THREE.Scene;
+  mouseCoords: MouseCoords = new THREE.Vector2();
+  selectedObject: THREE.Object3D | null = null;
+  offset: THREE.Vector3 = new THREE.Vector3();
+  onMouseMoveCallback?: EventCallback;
+  onClickCallback?: EventCallback;
+  //   onDblClickCallback?: EventCallback;
+  //   onMouseLeaveCallback?: EventCallback;
+  //   onDragCallback?: EventCallback;
+
+  constructor(camera: THREE.Camera, scene: THREE.Scene) {
+    super();
+    this.camera = camera;
+    this.scene = scene;
+  }
+
+  // 기본 동작: 마우스 좌표값 업데이트, 레이캐스팅 통해 교차점 찾기
+  updateMouseCoords(event: MouseEvent) {
+    this.mouseCoords.x = (event.clientX / window.innerWidth) * 2 - 1;
+    this.mouseCoords.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  }
+
+  getIntersects(event: MouseEvent): THREE.Intersection[] {
+    this.updateMouseCoords(event);
+    this.setFromCamera(this.mouseCoords, this.camera);
+    return this.intersectObjects(this.scene.children);
+  }
+
+  // 이벤트 핸들러에 콜백 등록
+  setMouseMoveHandler(mouseMoveCallback: EventCallback) {
+    this.onMouseMoveCallback = mouseMoveCallback;
+  }
+
+  setClickHandler(clickCallback: EventCallback) {
+    this.onClickCallback = clickCallback;
+  }
+
+  // 이벤트 핸들러 설정
+  handleEvent(event: MouseEvent, callback?: EventCallback) {
+    if (callback) {
+      const intersects = this.getIntersects(event);
+      callback(event, intersects);
+    }
+  }
+
+  onMouseMove(event: MouseEvent) {
+    this.handleEvent(event, this.onMouseMoveCallback);
+  }
+
+  onClick(event: MouseEvent) {
+    this.handleEvent(event, this.onClickCallback);
+  }
+}
