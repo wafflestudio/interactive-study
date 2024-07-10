6182c7b4194fe60bb703981b95b82b4823b1943f
Hyeonji Shin <wl77886@gmail.com>
2024-07-11 00:42:43 +0900

    change public dir (#24)

--- projects/find-waffle/vite.config.ts
+++ projects/find-waffle/vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  publicDir: './static/',
   base: mode === 'production' ? '/projects/find-waffle/' : '/',
 }));
