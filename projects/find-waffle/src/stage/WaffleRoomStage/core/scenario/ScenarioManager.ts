41645d2aa2d0d834a90827ab11f89f99ee37258b
designDefined <designdefined.by@gmail.com>
2024-07-22 23:10:05 +0900

    Fix/scenario manager (#38)
    * fix: Change scenario manager interface
    
    * fix: Change import

--- projects/find-waffle/src/stage/WaffleRoomStage/core/scenario/ScenarioManager.ts
+++ projects/find-waffle/src/stage/WaffleRoomStage/core/scenario/ScenarioManager.ts
@@ -2,33 +2,43 @@ import * as THREE from 'three';
 
 type Plot = {
   name: string;
-  onMount: () => void;
-  onUnmount: () => void;
+  onMount: (renderer: THREE.WebGLRenderer, camera: THREE.Camera) => void;
+  onUnmount: (renderer: THREE.WebGLRenderer, camera: THREE.Camera) => void;
 };
 
 export class ScenarioManager {
-  currentPlot?: Plot;
+  renderer: THREE.WebGLRenderer;
+  camera: THREE.Camera;
   plots: Plot[] = [];
+  currentPlot?: Plot;
 
-  constructor() {}
+  constructor(renderer: THREE.WebGLRenderer, camera: THREE.Camera) {
+    this.renderer = renderer;
+    this.camera = camera;
+  }
 
-  addPlot(name: string, onMount: () => {}, onUnmount: () => {}) {
+  addPlot(
+    name: string,
+    onMount: (renderer: THREE.WebGLRenderer, camera: THREE.Camera) => {},
+    onUnmount: (renderer: THREE.WebGLRenderer, camera: THREE.Camera) => {},
+  ) {
     this.plots.push({ name, onMount, onUnmount });
   }
 
   changePlot(name: string) {
     const target = this.plots.find((plot) => plot.name === name);
     if (!target) throw new Error(`Plot ${name} not found`);
-    if (this.currentPlot) this.currentPlot.onUnmount();
+    if (this.currentPlot)
+      this.currentPlot.onUnmount(this.renderer, this.camera);
     this.currentPlot = target;
-    this.currentPlot.onMount();
+    this.currentPlot.onMount(this.renderer, this.camera);
   }
 
   startPlot(name: string) {
     const target = this.plots.find((plot) => plot.name === name);
     if (!target) throw new Error(`Plot ${name} not found`);
-    if (this.currentPlot) this.currentPlot.onUnmount();
-    this.changePlot(name);
+    this.currentPlot = target;
+    this.currentPlot.onMount(this.renderer, this.camera);
   }
 
   isPlot(name: string) {
