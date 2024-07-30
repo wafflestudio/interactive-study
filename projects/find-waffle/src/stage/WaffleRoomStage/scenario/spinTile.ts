8a9d137b3ac8f332c1353cc55d68205c11513690
nuagenic <nuagenic@gmail.com>
2024-07-31 04:45:58 +0900

    feat: Get playerDirection

--- projects/find-waffle/src/stage/WaffleRoomStage/scenario/spinTile.ts
+++ projects/find-waffle/src/stage/WaffleRoomStage/scenario/spinTile.ts
@@ -1,3 +1,4 @@
+import * as CANNON from 'cannon-es';
 import { gsap } from 'gsap';
 import * as THREE from 'three';
 import { object } from 'zod';
@@ -29,6 +30,7 @@ export const spinTileScenario =
       {
         name: 'spintile_02', // 캐릭터가 타일을 돌려보는 씬
         onMount: () => {
+          let isAnimating = false;
           function pivotOnWorldAxis(
             object: THREE.Object3D,
             center: THREE.Vector3,
@@ -60,12 +62,41 @@ export const spinTileScenario =
           const tilePosition = { y: 0 };
 
           const player = cannonManager.totalObjectMap.get('Scene');
-          console.log(player);
 
           keyMap.bind('h', () => {
-            console.log('h');
+            console.log(player);
+
+            // Euler 각도로 변환하여 범위로 상하좌우 계산
+            const quaternion = player!.body.quaternion;
+            const euler = new CANNON.Vec3();
+            quaternion.toEuler(euler, 'YZX');
+            const yaw = euler.y * (180 / Math.PI);
+            console.log(yaw);
+
+            let playerDirection = '';
+            const errorRange = 20;
+
+            const downCase = yaw >= -errorRange && yaw <= errorRange;
+            const upCase = yaw >= 180 - errorRange || yaw <= -180 + errorRange; // 180도와 -180도 사이의 오차 허용
+            const rightCase = yaw >= 90 - errorRange && yaw <= 90 + errorRange;
+            const leftCase = yaw >= -90 - errorRange && yaw <= -90 + errorRange;
+
+            // 방향 판단을 위한 조건문
+            if (upCase) {
+              playerDirection = 'up';
+            } else if (downCase) {
+              playerDirection = 'down';
+            } else if (rightCase) {
+              playerDirection = 'right';
+            } else if (leftCase) {
+              playerDirection = 'left';
+            } else {
+              playerDirection = 'unknown';
+            }
+
+            if (isAnimating) return;
+
             const currentPosition = player!.body.position;
-            console.log(tileMap.keys());
             tileMap.forEach((textName, tileName) => {
               const tile = cannonManager.totalObjectMap.get(tileName);
               const text = cannonManager.totalObjectMap.get(textName);
@@ -77,11 +108,13 @@ export const spinTileScenario =
                 currentPosition.z <= tile!.boxCenter.z + halfExtents.z &&
                 currentPosition.z >= tile!.boxCenter.z - halfExtents.z
               ) {
+                isAnimating = true;
                 gsap.to(tilePosition, {
                   duration: 1,
                   y: 3,
                   ease: 'power2.inOut',
                   onUpdate: () => {
+                    // 타일 들어올리기
                     tile!.mesh.position.y = tilePosition.y;
                     text!.mesh.position.y = tilePosition.y;
                   },
@@ -91,6 +124,7 @@ export const spinTileScenario =
                       angle: 90,
                       ease: 'power2.inOut',
                       onUpdate: () => {
+                        // 타일 돌리기
                         const deltaAngle =
                           currentAngle.angle - tile!.mesh.userData.lastAngle;
                         pivotOnWorldAxis(
@@ -117,8 +151,12 @@ export const spinTileScenario =
                           y: 0,
                           ease: 'power2.inOut',
                           onUpdate: () => {
+                            // 타일 내리기
                             tile!.mesh.position.y = tilePosition.y;
                             text!.mesh.position.y = tilePosition.y;
+                          },
+                          onComplete: () => {
+                            isAnimating = false;
                           },
                         });
                       },
