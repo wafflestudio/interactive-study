253e31517d0829892c77e38d967a28d8a24cec68
nuagenic <nuagenic@gmail.com>
2024-07-30 17:05:15 +0900

    feat: Make tiles spin with letters

--- projects/find-waffle/src/stage/WaffleRoomStage/scenario/spinTile.ts
+++ projects/find-waffle/src/stage/WaffleRoomStage/scenario/spinTile.ts
@@ -29,35 +29,99 @@ export const spinTileScenario =
       {
         name: 'spintile_02', // 캐릭터가 타일을 돌려보는 씬
         onMount: () => {
+          function pivotOnWorldAxis(
+            object: THREE.Object3D,
+            center: THREE.Vector3,
+            axis: THREE.Vector3,
+            angle: number,
+          ): void {
+            const parent = object.parent;
+
+            if (parent === null) {
+              console.error("object can't be found in the world");
+            } else {
+              const radianAngle = THREE.MathUtils.degToRad(angle);
+
+              // rotate object on world axis
+              object.rotateOnWorldAxis(axis, radianAngle);
+
+              // rotate object position around center
+              parent.worldToLocal(
+                parent
+                  .localToWorld(object.position)
+                  .sub(center)
+                  .applyAxisAngle(axis, radianAngle)
+                  .add(center),
+              );
+            }
+          }
+
+          const currentAngle = { angle: 0 };
+          const tilePosition = { y: 0 };
+
           const player = cannonManager.totalObjectMap.get('Scene');
           console.log(player);
-
-          tileNames.forEach((tileName) => {
-            const tile = cannonManager.totalObjectMap.get(tileName);
-            cannonManager.filterCollision(tile?.body!, 2, 1);
-          });
 
           keyMap.bind('h', () => {
             console.log('h');
             const currentPosition = player!.body.position;
-            tileNames.forEach((tileName) => {
+            console.log(tileMap.keys());
+            tileMap.forEach((textName, tileName) => {
               const tile = cannonManager.totalObjectMap.get(tileName);
+              const text = cannonManager.totalObjectMap.get(textName);
               const halfExtents = tile?.body.shapes[0].halfExtents;
+
               if (
-                currentPosition.x <= tile.boxCenter.x + halfExtents.x &&
-                currentPosition.x >= tile.boxCenter.x - halfExtents.x &&
-                currentPosition.z <= tile.boxCenter.z + halfExtents.z &&
-                currentPosition.z >= tile.boxCenter.z - halfExtents.z
+                currentPosition.x <= tile!.boxCenter.x + halfExtents.x &&
+                currentPosition.x >= tile!.boxCenter.x - halfExtents.x &&
+                currentPosition.z <= tile!.boxCenter.z + halfExtents.z &&
+                currentPosition.z >= tile!.boxCenter.z - halfExtents.z
               ) {
-                gsap.to(tile?.mesh.position!, {
-                  duration: 2,
+                gsap.to(tilePosition, {
+                  duration: 1,
                   y: 3,
                   ease: 'power2.inOut',
+                  onUpdate: () => {
+                    tile!.mesh.position.y = tilePosition.y;
+                    text!.mesh.position.y = tilePosition.y;
+                  },
                   onComplete: () => {
-                    gsap.to(tile?.mesh.quaternion!, {
-                      duration: 2,
-                      y: Math.PI / 2,
+                    gsap.to(currentAngle, {
+                      duration: 1,
+                      angle: 90,
                       ease: 'power2.inOut',
+                      onUpdate: () => {
+                        const deltaAngle =
+                          currentAngle.angle - tile!.mesh.userData.lastAngle;
+                        pivotOnWorldAxis(
+                          tile!.mesh,
+                          tile!.boxCenter,
+                          new THREE.Vector3(0, 1, 0).normalize(),
+                          deltaAngle,
+                        );
+                        pivotOnWorldAxis(
+                          text!.mesh,
+                          text!.boxCenter,
+                          new THREE.Vector3(0, 1, 0).normalize(),
+                          deltaAngle,
+                        );
+                        tile!.mesh.userData.lastAngle = currentAngle.angle;
+                      },
+                      onStart: () => {
+                        tile!.mesh.userData.lastAngle = 0;
+                      },
+                      onComplete: () => {
+                        currentAngle.angle = 0;
+                        gsap.to(tilePosition, {
+                          duration: 1,
+                          y: 0,
+                          ease: 'power2.inOut',
+                          onUpdate: () => {
+                            tile!.mesh.position.y = tilePosition.y;
+                            text!.mesh.position.y = tilePosition.y;
+                          },
+                        });
+                      },
                     });
                   },
                 });
@@ -77,21 +141,7 @@ export const spinTileScenario =
     ];
   };
 
-const tileNames = [
-  '큐브098',
-  '큐브030',
-  '큐브038',
-  '큐브046',
-  '큐브054',
-  '큐브062',
-  '큐브070',
-  '큐브020',
-  '큐브028',
-  '큐브031',
-  '큐브039',
-  '큐브047',
-  '큐브055',
-  '큐브063',
-  '큐브071',
-  '큐브018',
-];
+const tileMap: Map<string, string> = new Map([
+  ['큐브098', '텍스트022'],
+  ['큐브030', '텍스트038'],
+]);
