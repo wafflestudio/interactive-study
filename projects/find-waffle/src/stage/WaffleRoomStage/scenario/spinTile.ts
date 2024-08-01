649234a219564dfb5866ea0084533bf34f4cd608
nuagenic <nuagenic@gmail.com>
2024-08-01 18:27:18 +0900

    feat: Complete tile spinning logic

--- projects/find-waffle/src/stage/WaffleRoomStage/scenario/spinTile.ts
+++ projects/find-waffle/src/stage/WaffleRoomStage/scenario/spinTile.ts
@@ -18,6 +18,7 @@ export const spinTileScenario =
     renderer: THREE.WebGLRenderer,
   ): Scenario =>
   (set) => {
+    const tileMap: Map<string, string> = new Map();
     return [
       {
         name: 'spintile_01', // 오프닝
@@ -30,7 +31,27 @@ export const spinTileScenario =
       {
         name: 'spintile_02', // 캐릭터가 타일을 돌려보는 씬
         onMount: () => {
+          for (let i = 1; i < 9; i++) {
+            for (let j = 1; j < 9; j++) {
+              const tileName = `tile_${i}${j}`;
+              const textName = `letter_${i}${j}`;
+              tileMap.set(tileName, textName);
+            }
+          }
+          tileMap.forEach((textName, tileName) => {
+            const tile = cannonManager.totalObjectMap.get(tileName);
+            tile!.mesh.userData.spinCount = 0;
+          });
+
+          const tile_w = cannonManager.totalObjectMap.get('tile_75');
+          const tile_a = cannonManager.totalObjectMap.get('tile_65');
+          const tile_f1 = cannonManager.totalObjectMap.get('tile_55');
+          const tile_f2 = cannonManager.totalObjectMap.get('tile_45');
+          const tile_l = cannonManager.totalObjectMap.get('tile_35');
+          const tile_e = cannonManager.totalObjectMap.get('tile_25');
+
           let isAnimating = false;
+
           function pivotOnWorldAxis(
             object: THREE.Object3D,
             center: THREE.Vector3,
@@ -64,15 +85,13 @@ export const spinTileScenario =
           const player = cannonManager.totalObjectMap.get('Scene');
 
           keyMap.bind('h', () => {
-            console.log(player);
-
             // Euler 각도로 변환하여 범위로 상하좌우 계산
             const quaternion = player!.body.quaternion;
             const euler = new CANNON.Vec3();
             quaternion.toEuler(euler, 'YZX');
             const yaw = euler.y * (180 / Math.PI);
 
-            let playerDirection = '';
+            let playerDirection = 1;
             const errorRange = 20;
 
             const downCase = yaw >= -errorRange && yaw <= errorRange;
@@ -82,15 +101,15 @@ export const spinTileScenario =
 
             // 방향 판단을 위한 조건문
             if (upCase) {
-              playerDirection = 'up';
+              playerDirection = 0; // up
             } else if (downCase) {
-              playerDirection = 'down';
+              playerDirection = 1; // down
             } else if (rightCase) {
-              playerDirection = 'right';
+              playerDirection = 2; // right
             } else if (leftCase) {
-              playerDirection = 'left';
+              playerDirection = 3; // left
             } else {
-              playerDirection = 'unknown';
+              playerDirection = 4;
             }
 
             if (isAnimating) return;
@@ -98,7 +117,6 @@ export const spinTileScenario =
             const currentPosition = player!.body.position;
             tileMap.forEach((textName, tileName) => {
               const tile = cannonManager.totalObjectMap.get(tileName);
-              const text = cannonManager.totalObjectMap.get(textName);
               const halfExtents = tile?.body.shapes[0].halfExtents;
 
               if (
@@ -107,15 +125,54 @@ export const spinTileScenario =
                 currentPosition.z <= tile!.boxCenter.z + halfExtents.z &&
                 currentPosition.z >= tile!.boxCenter.z - halfExtents.z
               ) {
+                const rowNum = Number(tileName.split('_')[1][0]);
+                const colNum = Number(tileName.split('_')[1][1]);
+                let targetTileName = '';
+                let targetTextName = '';
+                switch (playerDirection) {
+                  case 0:
+                    targetTileName = `tile_${rowNum - 1}${colNum}`;
+                    targetTextName = `letter_${rowNum - 1}${colNum}`;
+                    break;
+                  case 1:
+                    targetTileName = `tile_${rowNum + 1}${colNum}`;
+                    targetTextName = `letter_${rowNum + 1}${colNum}`;
+                    break;
+                  case 2:
+                    targetTileName = `tile_${rowNum}${colNum + 1}`;
+                    targetTextName = `letter_${rowNum}${colNum + 1}`;
+                    break;
+                  case 3:
+                    targetTileName = `tile_${rowNum}${colNum - 1}`;
+                    targetTextName = `letter_${rowNum}${colNum - 1}`;
+                    break;
+                }
+                const targetTile =
+                  cannonManager.totalObjectMap.get(targetTileName);
+                const targetText =
+                  cannonManager.totalObjectMap.get(targetTextName);
+                const targetTileParent = targetTile!.mesh.parent;
+                const targetTextParent = targetText!.mesh.parent;
+                const targetTileWorldPos = targetTile!.mesh.getWorldPosition(
+                  new THREE.Vector3(),
+                );
+                const targetTextWorldPos = targetText!.mesh.getWorldPosition(
+                  new THREE.Vector3(),
+                );
+
                 isAnimating = true;
                 gsap.to(tilePosition, {
                   duration: 1,
-                  y: 3,
+                  y: 2,
                   ease: 'power2.inOut',
                   onUpdate: () => {
                     // 타일 들어올리기
-                    tile!.mesh.position.y = tilePosition.y;
-                    text!.mesh.position.y = tilePosition.y;
+                    sceneManager.currentScene.attach(targetTile!.mesh);
+                    sceneManager.currentScene.attach(targetText!.mesh);
+                    targetTile!.mesh.position.y =
+                      targetTileWorldPos.y + tilePosition.y;
+                    targetText!.mesh.position.y =
+                      targetTextWorldPos.y + tilePosition.y;
                   },
                   onComplete: () => {
                     gsap.to(currentAngle, {
@@ -125,23 +182,25 @@ export const spinTileScenario =
                       onUpdate: () => {
                         // 타일 돌리기
                         const deltaAngle =
-                          currentAngle.angle - tile!.mesh.userData.lastAngle;
+                          currentAngle.angle -
+                          targetTile!.mesh.userData.lastAngle;
                         pivotOnWorldAxis(
-                          tile!.mesh,
-                          tile!.boxCenter,
+                          targetTile!.mesh,
+                          targetTile!.boxCenter,
                           new THREE.Vector3(0, 1, 0).normalize(),
                           deltaAngle,
                         );
                         pivotOnWorldAxis(
-                          text!.mesh,
-                          text!.boxCenter,
+                          targetText!.mesh,
+                          targetText!.boxCenter,
                           new THREE.Vector3(0, 1, 0).normalize(),
                           deltaAngle,
                         );
-                        tile!.mesh.userData.lastAngle = currentAngle.angle;
+                        targetTile!.mesh.userData.lastAngle =
+                          currentAngle.angle;
                       },
                       onStart: () => {
-                        tile!.mesh.userData.lastAngle = 0;
+                        targetTile!.mesh.userData.lastAngle = 0;
                       },
                       onComplete: () => {
                         currentAngle.angle = 0;
@@ -151,11 +210,29 @@ export const spinTileScenario =
                           ease: 'power2.inOut',
                           onUpdate: () => {
                             // 타일 내리기
-                            tile!.mesh.position.y = tilePosition.y;
-                            text!.mesh.position.y = tilePosition.y;
+                            targetTile!.mesh.position.y =
+                              targetTileWorldPos.y + tilePosition.y;
+                            targetText!.mesh.position.y =
+                              targetTextWorldPos.y + tilePosition.y;
                           },
                           onComplete: () => {
+                            targetTileParent!.attach(targetTile!.mesh);
+                            targetTextParent!.attach(targetText!.mesh);
                             isAnimating = false;
+                            targetTile!.mesh.userData.spinCount += 1;
+                            console.log(targetTile!.mesh.userData.spinCount);
+
+                            if (
+                              tile_w!.mesh.userData.spinCount % 4 === 2 &&
+                              tile_a!.mesh.userData.spinCount % 4 === 3 &&
+                              tile_f1!.mesh.userData.spinCount % 4 === 2 &&
+                              tile_f2!.mesh.userData.spinCount % 4 === 2 &&
+                              tile_l!.mesh.userData.spinCount % 4 === 3 &&
+                              tile_e!.mesh.userData.spinCount % 4 === 1
+                            ) {
+                              console.log('done');
+                              set('spintile_03');
+                            }
                           },
                         });
                       },
@@ -169,7 +246,9 @@ export const spinTileScenario =
       },
       {
         name: 'spintile_03',
-        onMount: () => {},
+        onMount: () => {
+          dialogue.begin(['와플을 찾았습니다!'], () => {});
+        },
       },
       {
         name: 'spintile_04',
@@ -177,8 +256,3 @@ export const spinTileScenario =
       },
     ];
   };
-
-const tileMap: Map<string, string> = new Map([
-  ['큐브098', '텍스트022'],
-  ['큐브030', '텍스트038'],
-]);
