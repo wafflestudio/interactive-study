085cf4b9e14a8bd23424b974e4b38e2a877d034b
designDefined <designdefined.by@gmail.com>
2024-08-02 00:44:38 +0900

    wip

---
+++ projects/find-waffle/src/stage/WaffleRoomStage/core/dialogue/components.ts
@@ -0,0 +1,67 @@
+import van from 'vanjs-core';
+
+import './styles.css';
+
+const { div, img, span } = van.tags;
+
+// types
+export type Chunk = {
+  size: 'normal' | 'large' | 'huge';
+  value: string;
+};
+
+export type DialogueState = {
+  currentChunks: Chunk[];
+  currentIndex: number;
+  lastIndex: number;
+};
+
+// util
+export const SlicedChunks = ({
+  currentChunks,
+  currentIndex,
+}: DialogueState) => {
+  const { slicedChunks } = currentChunks.reduce(
+    (acc, chunk) => {
+      if (acc.remainingCounts === 0) {
+        const newChunk = {
+          ...chunk,
+          value: ' ',
+        };
+        acc.slicedChunks.push(newChunk);
+        return acc;
+      }
+      if (acc.remainingCounts > chunk.value.length) {
+        acc.remainingCounts = acc.remainingCounts - chunk.value.length;
+        acc.slicedChunks.push(chunk);
+        return acc;
+      }
+      const newChunk = {
+        ...chunk,
+        value: chunk.value.slice(0, acc.remainingCounts),
+      };
+      acc.slicedChunks.push(newChunk);
+      acc.remainingCounts = 0;
+      return acc;
+    },
+    {
+      slicedChunks: [] as Chunk[],
+      remainingCounts: currentIndex,
+    },
+  );
+  return slicedChunks.map(({ size, value }) => span({ class: size }, value));
+};
+
+// states
+export const containerVisibility = van.state(false);
+
+// Container
+const containerClassName = van.derive(() =>
+  containerVisibility.val ? `DialogueContainer` : `DialogueContainer invisible`,
+);
+
+export const DialogueContainer = div(
+  { class: containerClassName },
+  img({ src: '/ui/dialogue_ui.png' }),
+  div({ class: 'textsContainer' }),
+);
