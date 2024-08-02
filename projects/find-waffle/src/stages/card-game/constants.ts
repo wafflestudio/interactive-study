2b7a8602ad60394b34e0ce3a6b57182bf788b7c3
nyanxyz <wl77886@gmail.com>
2024-08-02 22:26:51 +0900

    feat: implement card game stage

---
+++ projects/find-waffle/src/stages/card-game/constants.ts
@@ -0,0 +1,107 @@
+export const transform = {
+  object: {
+    rotateY: -Math.PI / 2,
+  },
+  tablecloth: {
+    position: {
+      z: 3.71, // tableSize.z
+    },
+  },
+  card: {
+    scale: {
+      x: 1,
+      y: 0.7,
+      z: 0.7,
+    },
+  },
+  z0: {
+    position: {
+      z: 3.705,
+    },
+  },
+  z1: {
+    position: {
+      z: 3.71,
+    },
+  },
+  z2: {
+    position: {
+      z: 3.715,
+    },
+  },
+  z3: {
+    position: {
+      z: 3.72,
+    },
+  },
+  z4: {
+    position: {
+      z: 3.725,
+    },
+  },
+  z5: {
+    position: {
+      z: 3.87,
+    },
+  },
+
+  heartQueen: {
+    position: {
+      x: 0,
+      y: 0.9,
+    },
+  },
+  spadeKing: {
+    position: {
+      x: 0.4,
+      y: 0.9,
+    },
+  },
+  diaJack: {
+    position: {
+      x: 0.8,
+      y: 0.9,
+    },
+  },
+  clubQueen: {
+    position: {
+      x: 1.2,
+      y: 0.9,
+    },
+  },
+
+  diaKing: {
+    position: {
+      x: -0.8,
+      y: 0.3,
+    },
+  },
+  heartKing: {
+    position: {
+      x: -0.4,
+      y: 0.3,
+    },
+  },
+  clubKing: {
+    position: {
+      x: 0.8,
+      y: 0.3,
+    },
+  },
+
+  diaQueen: {
+    position: {
+      x: 0.8,
+      y: -0.8,
+    },
+    rotateY: -Math.PI / 2,
+    rotateX: -Math.PI / 5,
+  },
+
+  sword: {
+    position: {
+      x: 0.505,
+      y: 0.89,
+    },
+  },
+};
