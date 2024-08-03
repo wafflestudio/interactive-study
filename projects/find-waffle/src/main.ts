9e26d5a7d3fda0dd32676ec491b0eca0ce7b8a30
nuagenic <nuagenic@gmail.com>
2024-08-03 16:20:44 +0900

    feat: Add waffleRoom to main.ts

--- projects/find-waffle/src/main.ts
+++ projects/find-waffle/src/main.ts
@@ -1,23 +1,29 @@
 import { StageManager } from './core/stage/StageManager';
-import TestBlueStage from './example/TestBlueStage';
 import TestHomeStage from './example/TestHomeStage';
 import WaffleRoomStage from './stage/WaffleRoomStage';
+import CardGameStage from './stages/card-game';
+import EscapeCubeStage from './stages/escape-cube';
 import { GooseStage } from './stages/goose/stage';
 
 // Stages
 const stageManager = StageManager.instance;
 const gooseStage = new GooseStage(stageManager.renderer, stageManager.app);
-const testBlue = new TestBlueStage(stageManager.renderer, stageManager.app);
+const escapeCubeStage = new EscapeCubeStage(
+  stageManager.renderer,
+  stageManager.app,
+);
+const cardGame = new CardGameStage(stageManager.renderer, stageManager.app);
+const waffleRoom = new WaffleRoomStage(stageManager.renderer, stageManager.app);
 const testHome = new TestHomeStage(stageManager.renderer, stageManager.app, [
   gooseStage,
-  testBlue,
+  escapeCubeStage,
+  cardGame,
+  waffleRoom,
 ]);
 
-const waffleRoom = new WaffleRoomStage(stageManager.renderer, stageManager.app);
-
-stageManager.setHome(waffleRoom);
-window.requestAnimationFrame((t) => stageManager.animate(t));
+stageManager.setHome(testHome);
+window.requestAnimationFrame(() => stageManager.animate());
 window.addEventListener('resize', (e) => stageManager.resize(e));
 
 // 디버깅용
-// stageManager.toStage(gooseStage);
+stageManager.toStage(gooseStage);
