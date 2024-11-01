782ed17ecf435d5471525eae3975efa7000540bd
Minkyu Lee <whizkyu@snu.ac.kr>
2024-11-02 04:00:08 +0900

    feat: vite plugin initial settings

---
+++ projects/web-filter/src/pages/Popup.tsx
@@ -0,0 +1,16 @@
+import { useEffect } from 'react';
+import "./Popup.css";
+
+export default function() {
+  useEffect(() => {
+    console.log("Hello from the popup!");
+  }, []);
+
+  return (
+    <div>
+      <img src="/coffee-filter.png" />
+      <h1>Web Filter</h1>
+      <p>by. Team Seoee - Interactive Study</p>
+    </div>
+  )
+}
