51f269ceb0c77e721250da947959e77d92d691dd
lerrybe <yeji.kim@dalpha.so>
2024-11-07 21:35:55 +0900

    feat: tab message 전송부 구현

--- projects/web-filter/src/background.ts
+++ projects/web-filter/src/background.ts
@@ -5,3 +5,7 @@ console.log('Hello from the background!');
 browser.runtime.onInstalled.addListener((details) => {
   console.log('Extension installed:', details);
 });
+
+chrome.runtime.onMessage.addListener((message) => {
+  chrome.runtime.sendMessage(message);
+});
--- projects/web-filter/src/pages/Popup.tsx
+++ projects/web-filter/src/pages/Popup.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 
+import { MessageAction } from '../helpers/types/message';
 import './Popup.css';
 
 export default function () {
@@ -7,11 +8,72 @@ export default function () {
     console.log('Hello from the popup!');
   }, []);
 
+  /**
+   * @context chrome.tabs
+   * https://developer.chrome.com/docs/extensions/mv2/reference/tabs?hl=ko
+   */
+
+  // 테스트
+  const getCurrentTab = async () => {
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  };
+
+  const applyFilter = async (filter: string) => {
+    const tab = await getCurrentTab();
+    if (tab?.id) {
+      chrome.tabs.sendMessage(tab.id, {
+        action: MessageAction.APPLY_FILTER,
+        filter, // filter 정보를 담아서 전송 {key: filter}
+      });
+    }
+  };
+
+  const startSelection = async () => {
+    const tab = await getCurrentTab();
+    if (tab?.id) {
+      chrome.tabs.sendMessage(tab.id, {
+        action: MessageAction.START_SELECTION,
+      });
+    }
+  };
+
+  // TODO: (테스트) 인라인 스타일은 후에 css 파일로 분리
   return (
     <div>
       <img src="/coffee-filter.png" />
       <h1>Web Filter</h1>
-      <p>by. Team Seoee - Interactive Study</p>
+      <button
+        onClick={startSelection}
+        style={{
+          width: '100px',
+          height: '30px',
+        }}
+      >
+        요소 선택
+      </button>
+
+      <section style={{ display: 'flex', gap: '10px' }}>
+        <button
+          onClick={() => applyFilter('filter')}
+          style={{
+            width: '100px',
+            height: '30px',
+          }}
+        >
+          Wave Filter
+        </button>
+        <button
+          onClick={() => applyFilter('vintageFilter')}
+          style={{
+            width: '100px',
+            height: '30px',
+          }}
+        >
+          Vintage Filter
+        </button>
+      </section>
     </div>
   );
 }
