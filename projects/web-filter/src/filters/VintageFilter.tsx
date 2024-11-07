ac27cdc0e069089e42553c5a83a51b855b4bf9b3
lerrybe <yeji.kim@dalpha.so>
2024-11-07 21:54:23 +0900

    feat: 필터 수정

--- projects/web-filter/src/filters/VintageFilter.tsx
+++ projects/web-filter/src/filters/VintageFilter.tsx
@@ -1,17 +1,6 @@
 export function VintageFilter() {
   return (
     <filter id="vintageFilter">
-      {/* 세피아 톤 추가 */}
-      <feColorMatrix
-        type="matrix"
-        values="
-          0.393 0.769 0.189 0 0
-          0.349 0.686 0.168 0 0
-          0.272 0.534 0.131 0 0
-          0     0     0     1 0"
-        result="sepia"
-      />
-
       {/* 그레인 효과 추가 */}
       <feTurbulence
         type="fractalNoise"
