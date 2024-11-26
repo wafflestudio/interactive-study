f64e1ad1a8495495fb812c55655f115e9a94dfd3
lerrybe <yeji.kim@dalpha.so>
2024-11-27 08:12:56 +0900

    feat: action 분리

--- projects/web-filter/src/types/status.ts
+++ projects/web-filter/src/types/status.ts
@@ -3,3 +3,8 @@ export enum STATUS {
   SURFING = 'SURFING', // 마우스가 움직이면서 요소를 탐색 중인 상태
   SELECTED = 'SELECTED', // 요소가 선택된 상태
 }
+
+export enum ACTION {
+  START_SELECT_ELEMENT = 'START_SELECT_ELEMENT',
+  APPLY_FILTER = 'APPLY_FILTER',
+}
